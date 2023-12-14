00a4d856cef6d62f30e307150a045c0c623206c4
Lyusi Divakov <divakovwork@gmail.com>
2023-12-14 12:02:17 +0200

    ready database

--- RightPath/Data/ApplicationDbContext.cs
+++ RightPath/Data/ApplicationDbContext.cs
@@ -9,6 +9,7 @@ namespace RightPath.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
