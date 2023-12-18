f7d32523b6fba6a6370252b2b6f6772fd0b330ee
Lyusi Divakov <divakovwork@gmail.com>
2023-12-18 08:57:51 +0200

    Create courses Controller Model and View. The entity type 'CourseImage' was not found

---
+++ RightPath/Controllers/CoursesController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RightPath.Data;
+using RightPath.Models;
+using RightPath.Models.NewModelView;
+
+namespace RightPath.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+        public CoursesController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: Courses
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Courses.ToListAsync());
+        }
+
+        // GET: Courses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Courses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CourseImage course)
+        {
+            string uniqueFileName = UploadeFile(course);
+            if (ModelState.IsValid)
+            {
+                Course course1 = new Course()
+                {
+                    CourseTitle = course.CourseTitle,
+                    CourseDescription = course.CourseDescription,
+                    CourseDuration = course.CourseDuration,
+                    CourseLogo = uniqueFileName,
+                    Lecture1 = course.Lecture1,
+                    Lecture2 = course.Lecture2,
+
+                };
+
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        private string UploadeFile(CourseImage course)
+
+        {
+
+            string uniqueFileName = null;
+
+            if (course.CourseLogo!= null)
+            {
+
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "Images");
+
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + course.CourseLogo.FileName;
+
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                Console.WriteLine(filePath);
+
+
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+
+                {
+
+                    course.CourseLogo.CopyTo(fileStream);
+
+                }
+
+            }
+
+            return uniqueFileName;
+
+        }
+
+        // GET: Courses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return View(course);
+        }
+
+        // POST: Courses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CourseTitle,CourseDescription,CourseDuration,CourseLogo,Lecture1,Lecture2")] Course course)
+        {
+            if (id != course.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course != null)
+            {
+                _context.Courses.Remove(course);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RightPath/Models/NewModelView/CourseImage.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RightPath.Models.NewModelView
+{
+    public class CourseImage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Course Logo")]
+        [Required(ErrorMessage = "Course Logo is required")]
+        public IFormFile CourseLogo { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
+        public string CourseTitle { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Description must be between 3 and 50 chars")]
+        public string CourseDescription { get; set; }
+
+        [Display(Name = "Duration")]
+        [Required(ErrorMessage = "Duration is required and must be real hours!")]
+        public double CourseDuration { get; set; }
+
+        [Display(Name = "Lecture1")]
+        [Required(ErrorMessage = "Lecture1 is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Lecture1 name must be between 3 and 50 chars")]
+        public string Lecture1 { get; set; }
+
+        [Display(Name = "Lecture2")]
+        public string Lecture2 { get; set; }
+    }
+}
