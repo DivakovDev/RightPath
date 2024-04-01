5b763cab4558683b29691763f000a726cf932d57
Lyusi Divakov <divakovwork@gmail.com>
2024-04-01 16:37:57 +0300

    Add page ForUs

---
+++ RightPath/Areas/Customer/Controllers/ForUsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RightPath.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ForUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
